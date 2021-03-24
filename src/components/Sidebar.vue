55097814d9347963506a664f6e65717352db62d4
97Dmitry <dart_veyder_1997@mail.ru>
2021-03-25 01:51:28 +0600

    Was created components and added some functional

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,59 @@
+<template>
+  <ul class="sidenav app-sidenav" :class="{ open: modelValue }">
+    <router-link
+      :to="link.toGo"
+      :exact="link.exact"
+      v-for="link in Links"
+      :key="link.toGo"
+      v-slot="{ href, navigate, isActive }"
+      custom
+    >
+      <li :class="{ active: isActive }">
+        <a
+          :href="href"
+          @click="navigate"
+          class="waves-effect waves-orange pointer"
+        >
+          {{ link.title }}
+        </a>
+      </li>
+    </router-link>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "Sidebar",
+  props: ["modelValue"],
+  data() {
+    return {
+      Links: [
+        { title: "Счет", toGo: "/", exact: true },
+        { title: "История", toGo: "/history" },
+        { title: "Планирование", toGo: "/planning" },
+        { title: "Новая запись", toGo: "/record" },
+        { title: "Категории", toGo: "/categories" },
+      ],
+    };
+  },
+  created() {
+    // this.links();
+  },
+  methods: {
+    // links() {
+    //   this.Links = [
+    //     {title: 'Счет', toGo: '/', exact: true},
+    //     {title: 'История', toGo: '/history'},
+    //     {title: 'Планирование', toGo: '/planning'},
+    //     {title: 'Новая запись', toGo: '/record'},
+    //     {title: 'Категории', toGo: '/categories'},
+    //   ]
+    // },
+    log() {
+      console.log(this.value);
+    },
+  },
+};
+</script>
+
+<style scoped></style>
